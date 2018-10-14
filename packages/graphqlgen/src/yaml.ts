a219e099a8d1b36af7b607572b12cd5f976c6879
Flavian DESVERNE <flavian.desverne@epitech.eu>
2018-10-14 21:12:38 +0200

    Split into a few helpers

---
+++ packages/graphqlgen/src/yaml.ts
@@ -0,0 +1,32 @@
+import * as Ajv from 'ajv'
+import * as chalk from 'chalk';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml'
+
+import { GraphQLGenDefinition } from 'graphqlgen-json-schema';
+
+import schema = require('graphqlgen-json-schema/dist/schema.json')
+
+const ajv = new Ajv().addMetaSchema(
+  require('ajv/lib/refs/json-schema-draft-06.json'),
+)
+const validateYaml = ajv.compile(schema)
+
+export function parseConfig() {
+  if (!fs.existsSync('factory.yml')) {
+    console.error(chalk.default.red(`No factory.yml found`))
+    process.exit(1)
+  }
+
+  const config = yaml.safeLoad(
+    fs.readFileSync('factory.yml', 'utf-8'),
+  ) as GraphQLGenDefinition
+
+  //TODO: Provide better errors
+  if (!validateYaml(config)) {
+    console.error(chalk.default.red(`Invalid graphqlgen.yml file`))
+    process.exit(1)
+  }
+
+  return config
+}
