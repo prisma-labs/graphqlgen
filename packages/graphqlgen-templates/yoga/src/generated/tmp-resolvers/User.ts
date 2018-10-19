1f5c380864079dda4258328d00db5335b61f5e68
schickling <schickling.j@gmail.com>
2018-10-19 10:47:04 +0200

    updated import paths

--- packages/graphqlgen-templates/yoga/src/generated/tmp-resolvers/User.ts
+++ packages/graphqlgen-templates/yoga/src/generated/tmp-resolvers/User.ts
@@ -1,12 +1,12 @@
 // This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
 // Please do not import this file directly but copy & paste to your application code.
 
-import { UserResolvers } from "./src/generated/graphqlgen.ts";
+import { UserResolvers } from '../graphqlgen'
 
 export const User: UserResolvers.Type = {
   ...UserResolvers.defaultResolvers,
 
   posts: parent => {
-    throw new Error("Resolver not implemented");
-  }
-};
+    throw new Error('Resolver not implemented')
+  },
+}
