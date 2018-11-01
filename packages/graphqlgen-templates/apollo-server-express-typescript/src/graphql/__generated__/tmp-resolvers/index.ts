3f9772bbd236a512d3177a804f9969a09cb170c6
Vishnu Sankar <iamvishnusankar@live.com>
2018-11-02 05:18:42 +0530

    - Added apollo express example

---
+++ packages/graphqlgen-templates/apollo-server-express-typescript/src/graphql/__generated__/tmp-resolvers/index.ts
@@ -0,0 +1,16 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { Resolvers } from '../graphqlgen'
+
+import { Query } from './Query'
+import { Post } from './Post'
+import { User } from './User'
+import { Mutation } from './Mutation'
+
+export const resolvers: Resolvers = {
+  Query,
+  Post,
+  User,
+  Mutation,
+}
--- packages/graphqlgen-templates/apollo-server-express-typescript/src/index.ts
+++ packages/graphqlgen-templates/apollo-server-express-typescript/src/index.ts
@@ -1,9 +1,27 @@
+import { ApolloServer, gql } from 'apollo-server-express'
 import * as express from 'express'
+import { importSchema } from 'graphql-import'
+
 import { resolvers } from './graphql'
 import { data } from './data'
 
 const app = express()
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
+
+// Import the schema as string
+const schema = importSchema(
+  './src/graphql/__generated__/graphqlgen.schema.graphql',
+)
+
+const apolloServer = new ApolloServer({
+  typeDefs: gql(schema),
+  resolvers: resolvers as any,
+  context: { data },
+})
+
+apolloServer.applyMiddleware({
+  app,
+})
 
 app.listen(PORT, () => {
   console.log(`server started on http://localhost:${PORT}`)
