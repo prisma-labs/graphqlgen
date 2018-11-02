39ea1f1f9424d0cf3f2735d96a9796e4ced1b925
Vishnu Sankar <iamvishnusankar@live.com>
2018-11-02 05:52:14 +0530

    - Advance scaffold test

---
+++ packages/graphqlgen/src/tests/fixtures/advanced-scaffold/models/definitions.ts
@@ -0,0 +1,10 @@
+export interface Schema1 {
+  schema1: string
+}
+
+export interface Schema2 {
+  schema2: string
+}
+export interface Schema3 {
+  schema3: string
+}
--- packages/graphqlgen/src/tests/scaffold-typescript-advanced/advanced-scaffold.test.ts
+++ packages/graphqlgen/src/tests/scaffold-typescript-advanced/advanced-scaffold.test.ts
@@ -1,10 +1,21 @@
 import { createConfig } from 'graphqlgen-json-schema/dist/default-config'
 import { readSchemaSync, generateSchema } from '../../concat-schema'
+import { parseSchema, parseModels } from '../../parse'
+import { generateCode } from '../..'
+import { extractGlobPattern, handleGlobPattern } from '../../glob'
 
 const config = createConfig('./src/tests/fixtures/advanced-scaffold')
 
 test('create config', async () => {
   expect(config).toMatchSnapshot()
+})
+
+test('Check Schemas', () => {
+  expect(extractGlobPattern([config.schema])).toMatchSnapshot()
+})
+
+test('Check Models', () => {
+  expect(extractGlobPattern(config.models.files as any)).toMatchSnapshot()
 })
 
 test('Read Schema', () => {
@@ -13,4 +24,30 @@ test('Read Schema', () => {
 
 test('Generate Schema', () => {
   expect(generateSchema(config)).toBeTruthy()
+})
+
+test('Advance scaffolding', () => {
+  const language = config.language
+
+  generateSchema(config)
+  config.schema = config['schema-output']
+
+  // Override the config.models.files using handleGlobPattern
+  config.models = {
+    ...config.models,
+    files: handleGlobPattern(config.models.files),
+  }
+
+  const schema = parseSchema(config.schema)
+
+  const modelMap = parseModels(config.models, schema, config.output, language)
+  const { generatedTypes, generatedResolvers } = generateCode({
+    schema,
+    language,
+    config,
+    modelMap,
+    prettify: true,
+  })
+  expect(generatedTypes).toMatchSnapshot()
+  expect(generatedResolvers).toMatchSnapshot()
 })
