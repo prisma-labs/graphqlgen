cc1fd52bd61bf266fb92a51b35a55c55f4c05b7e
Flavian DESVERNE <flavian.desverne@epitech.eu>
2018-11-05 22:16:21 +0100

    Updated tests

--- packages/graphqlgen/src/tests/introspection/flow.test.ts
+++ packages/graphqlgen/src/tests/introspection/flow.test.ts
@@ -1,13 +1,14 @@
 import { join } from 'path'
-import { typeNamesFromFlowFile } from '../../introspection/flow-ast'
+import { buildTypesMap } from '../../introspection'
 
 const relative = (p: string) => join(__dirname, p)
+const language = 'flow'
 
 describe('flow file introspection', () => {
   test('find all types in file', () => {
-    const typesNames = typeNamesFromFlowFile({
-      path: relative('./mocks/flow-types.js'),
-    })
+    const typesNames = Object.keys(
+      buildTypesMap(relative('./mocks/flow-types.js'), language),
+    )
 
     expect(typesNames).toEqual([
       'Interface',
@@ -16,19 +17,4 @@ describe('flow file introspection', () => {
       'ExportedType',
     ])
   })
-
-  // test('extract fields from flow type', () => {
-  //   const model: Model = {
-  //     definition: 'Interface',
-  //     absoluteFilePath: relative('./mocks/flow-types.js'),
-  //     importPathRelativeToOutput: 'not_used'
-  //   }
-  //   const fields = extractFieldsFromFlowType(model)
-
-  //   expect(fields).toEqual([
-  //     { fieldName: 'field', fieldOptional: false },
-  //     { fieldName: 'optionalField', fieldOptional: true },
-  //     { fieldName: 'fieldUnionNull', fieldOptional: true },
-  //   ])
-  // })
 })
