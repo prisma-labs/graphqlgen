6e82d04a71d920587fd37d79d1f27e8c030d74cb
Jason Kuhrt <jasonkuhrt@me.com>
2018-12-13 23:22:04 -0500

    debug: only one basic test

--- packages/graphqlgen/src/tests/flow/basic.test.ts
+++ packages/graphqlgen/src/tests/flow/basic.test.ts
@@ -21,63 +21,12 @@ test('basic schema', async () => {
   })
 })
 
-test('basic enum', async () => {
-  return testGeneration({
-    language,
-    schema: relative('../fixtures/enum/schema.graphql'),
-    models: {
-      files: [relative('../fixtures/enum/types-flow.js')],
-    },
-    output: typesPath,
-    ['resolver-scaffolding']: {
-      output: resolversDir,
-      layout: 'file-per-type',
-    },
-  })
-})
-
-test('basic union', async () => {
-  return testGeneration({
-    language,
-    schema: relative('../fixtures/union/schema.graphql'),
-    models: {
-      files: [relative('../fixtures/union/flow-types.js')],
-    },
-    output: typesPath,
-    ['resolver-scaffolding']: {
-      output: resolversDir,
-      layout: 'file-per-type',
-    },
-  })
-})
-
-test('defaultName', async () => {
-  return testGeneration({
-    language,
-    schema: relative('../fixtures/defaultName/schema.graphql'),
-    models: {
-      files: [
-        {
-          path: relative('../fixtures/defaultName/flow-types.js'),
-          defaultName: '${typeName}Node',
-        },
-      ],
-    },
-    output: typesPath,
-    ['resolver-scaffolding']: {
-      output: resolversDir,
-      layout: 'file-per-type',
-    },
-  })
-})
-
-//TODO: Fix this test (detected since compiling flow)
-// test('basic scalar', async () => {
+// test('basic enum', async () => {
 //   return testGeneration({
 //     language,
-//     schema: relative('../fixtures/scalar/schema.graphql'),
+//     schema: relative('../fixtures/enum/schema.graphql'),
 //     models: {
-//       files: [relative('../fixtures/scalar/flow-types.js')],
+//       files: [relative('../fixtures/enum/types-flow.js')],
 //     },
 //     output: typesPath,
 //     ['resolver-scaffolding']: {
@@ -87,33 +36,84 @@ test('defaultName', async () => {
 //   })
 // })
 
-test('context', async () => {
-  return testGeneration({
-    language,
-    schema: relative('../fixtures/context/schema.graphql'),
-    context: relative('../fixtures/context/flow-types.js:Context'),
-    models: {
-      files: [relative('../fixtures/context/flow-types.js')],
-    },
-    output: typesPath,
-    ['resolver-scaffolding']: {
-      output: resolversDir,
-      layout: 'file-per-type',
-    },
-  })
-})
+// test('basic union', async () => {
+//   return testGeneration({
+//     language,
+//     schema: relative('../fixtures/union/schema.graphql'),
+//     models: {
+//       files: [relative('../fixtures/union/flow-types.js')],
+//     },
+//     output: typesPath,
+//     ['resolver-scaffolding']: {
+//       output: resolversDir,
+//       layout: 'file-per-type',
+//     },
+//   })
+// })
 
-test('subscription', () => {
-  return testGeneration({
-    language,
-    schema: relative('../fixtures/subscription/schema.graphql'),
-    models: {
-      files: [relative('../fixtures/subscription/flow-types.js')],
-    },
-    output: typesPath,
-    ['resolver-scaffolding']: {
-      output: resolversDir,
-      layout: 'file-per-type',
-    },
-  })
-})
+// test('defaultName', async () => {
+//   return testGeneration({
+//     language,
+//     schema: relative('../fixtures/defaultName/schema.graphql'),
+//     models: {
+//       files: [
+//         {
+//           path: relative('../fixtures/defaultName/flow-types.js'),
+//           defaultName: '${typeName}Node',
+//         },
+//       ],
+//     },
+//     output: typesPath,
+//     ['resolver-scaffolding']: {
+//       output: resolversDir,
+//       layout: 'file-per-type',
+//     },
+//   })
+// })
+
+// //TODO: Fix this test (detected since compiling flow)
+// // test('basic scalar', async () => {
+// //   return testGeneration({
+// //     language,
+// //     schema: relative('../fixtures/scalar/schema.graphql'),
+// //     models: {
+// //       files: [relative('../fixtures/scalar/flow-types.js')],
+// //     },
+// //     output: typesPath,
+// //     ['resolver-scaffolding']: {
+// //       output: resolversDir,
+// //       layout: 'file-per-type',
+// //     },
+// //   })
+// // })
+
+// test('context', async () => {
+//   return testGeneration({
+//     language,
+//     schema: relative('../fixtures/context/schema.graphql'),
+//     context: relative('../fixtures/context/flow-types.js:Context'),
+//     models: {
+//       files: [relative('../fixtures/context/flow-types.js')],
+//     },
+//     output: typesPath,
+//     ['resolver-scaffolding']: {
+//       output: resolversDir,
+//       layout: 'file-per-type',
+//     },
+//   })
+// })
+
+// test('subscription', () => {
+//   return testGeneration({
+//     language,
+//     schema: relative('../fixtures/subscription/schema.graphql'),
+//     models: {
+//       files: [relative('../fixtures/subscription/flow-types.js')],
+//     },
+//     output: typesPath,
+//     ['resolver-scaffolding']: {
+//       output: resolversDir,
+//       layout: 'file-per-type',
+//     },
+//   })
+// })
