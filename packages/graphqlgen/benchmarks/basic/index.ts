29af3d722ecfb8f40d8c2f8aec110916477e7d54
Jason Kuhrt <jasonkuhrt@me.com>
2018-12-24 16:35:43 -0500

    scaffold a models file

--- packages/graphqlgen/benchmarks/basic/index.ts
+++ packages/graphqlgen/benchmarks/basic/index.ts
@@ -3,12 +3,16 @@ import * as Sys from '../lib/sys'
 
 const log = console.log
 
+const language = 'typescript'
 const sdlFilePath = Sys.toAbsolutePath('./schema.graphql')
 const schema = Parse.parseSchema(sdlFilePath)
+const models = Sys.toAbsolutePath('./models.ts')
 
 const run = () => {
   log('will run basic bench')
   log(schema)
+  log(language)
+  log(models)
 }
 
 export { run }
